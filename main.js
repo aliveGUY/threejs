7d0bd19cd1d2b27f9fc432a0707c6b33111e79fc
Illia Dubrovin <75540413+aliveGUY@users.noreply.github.com>
2022-03-29 15:36:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 //import * as THREE from 'three';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+//import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
