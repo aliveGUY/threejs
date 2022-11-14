460410e12ff1e4f698a4919901072a2b9bef275b
Illia Dubrovin <ilyadubrivvin@gmail.com>
2022-11-14 15:17:27 +0200

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -3,103 +3,60 @@ import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGL1Renderer({
-  canvas: document.querySelector('#bg'),
+    canvas: document.querySelector('#bg'),
+    alpha: true,
 })
 
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-camera.position.setZ(30);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(10);
 
 renderer.render(scene, camera);
 
-//-----------------------------Geomery------------------------------------
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100)
-const material = new THREE.MeshStandardMaterial( { color: 0xFF6357 } );
-const torus = new THREE.Mesh( geometry, material );
-scene.add(torus)
-
 //-----------------------------Light------------------------------------
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set( 10,10,10 );
-const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight)
-
-
-
-function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({color:0xffffff})
-  const star = new THREE.Mesh(geometry,material);
-
-  const[x,y,z] = Array(3).fill().map(()=>THREE.MathUtils.randFloatSpread(100));
-
-  star.position.set(x,y,z);
-  scene.add(star)
-}
-Array(200).fill().forEach(addStar)
- 
-
-//background
-const spaceTexture = new THREE.TextureLoader().load('img/space.jpg');
-scene.background = spaceTexture;
+const pointLight = new THREE.PointLight(0xfff);
+pointLight.position.set(10, 10, 10);
 
 //avatar
-const avatarTexture = new THREE.TextureLoader().load('img/Avatar.jpg');
+const avatarTexture = new THREE.TextureLoader().load('Emblem_of_National_Aviation_University.png');
 
 const avatar = new THREE.Mesh(
-  new THREE.BoxGeometry(3,3,3),
-  new THREE.MeshBasicMaterial({map:avatarTexture})
+    new THREE.BoxGeometry(3, 3, 3),
+    new THREE.MeshBasicMaterial({ map: avatarTexture })
 );
 scene.add(avatar)
+const raycaster = new THREE.Raycaster();
+const pointer = new THREE.Vector2();
 
-//moon
-const moonTexture = new THREE.TextureLoader().load('img/moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('img/normal.jpg');
-
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3,32,32),
-  new THREE.MeshStandardMaterial({
-    map:moonTexture,
-    normalMap: normalTexture
-  })
-);
-scene.add(moon)
-
-moon.position.z = 30;
-moon.position.setX(-10);
-
-
-function moveCamera(){
-  const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
-
-  avatar.rotation.y += 0.01;
-  avatar.rotation.z += 0.01;
-
-  camera.position.x = t * -0.0002;
-  camera.position.y = t * -0.0002;
-  camera.position.z = t * -0.01;
-
+function onPointerMove( event ) {
+	pointer.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	pointer.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
-document.body.onscroll = moveCamera
-
 
 
 function animate() {
-  requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.01;
-  torus.rotation.z += 0.01;
+    avatar.rotation.x += 0.01;
 
-  //controls.update();
+    //controls.update();
+    raycaster.setFromCamera( pointer, camera );
+    var intersects = raycaster.intersectObject(scene, true);
+    if (intersects.length > 0) {
 
-  renderer.render(scene, camera);
+        var object = intersects[0].object;
+        
+        object.rotation.z += 0.01;
+
+    
+    }
+    
+
+    renderer.render(scene, camera);
 }
 
+window.addEventListener( 'pointermove', onPointerMove );
 animate();
